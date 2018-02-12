c134f1e4099f5a94f5a68144905a860f7a47d59d
eastman.jason.r@gmail.com <eastman.jason.r@gmail.com>
2018-02-12 11:26:48 -0700

    Finish initial structure and minimal animation

--- ind.js
+++ ind.js
@@ -1,5 +1,13 @@
 $(()=> {
-  console.log('If you are reading this: gooberfuludjflkasjdf!')
+
+console.log('If you are reading this: gooberfuludjflkasjdf!')
   $('.page').hide().fadeIn(1000)
+  
+  $(() => {
+    $(" .header > #block, .foot > #block").hover(()=> {
+      $(".foot > #block, .header > #block").effect("bounce", "slow")
+    })
+  })
+
 
 })
