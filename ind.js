c371b726eb8fe4cb9bb3d2a423e74b21805dc43b
eastman.jason.r@gmail.com <eastman.jason.r@gmail.com>
2018-02-12 14:55:17 -0700

    Make responsive, legible on devices.

--- ind.js
+++ ind.js
@@ -1,14 +1,18 @@
 $(()=> {
 
 console.log('If you are reading this: gooberfuludjflkasjdf!')
-  $('.page').hide().fadeIn(1000)
+  $(".page").hide().fadeIn(1000)
 
- /* $('.header > h2').hover(
-                    x= $(this).text()
-                    $(this).text("#4A5245"),
-                    ()=> {
-                      $(this).text(x)
-                    })
+  $("#change").hover(()=>{
+          oldt = $("#change").text()
+          $("#change").text("//4A5245")
+                  console.log(oldt)
+          },()=>{
+                  $("#change").text(oldt)
+                  console.log(oldt)
+          })
 
-*/
+
+
+
 })
